647c2bd91ede05de022134885789090958bbb437
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2020-12-05 19:05:29 +0000

    add new

--- src/index.ts
+++ src/index.ts
@@ -16,12 +16,17 @@ export interface IPropResolution {
     idx: number;
 
     /**
+     * Error message when failed.
+     */
+    errMsg?: string;
+
+    /**
      * Final resolved value, if successful, or else the property is not set.
      */
     value?: any;
 
     /**
-     * Last but one object/function in the chain, which is the target context for the final property's resolution.
+     * Last but one object in the chain, which is the target context for the final property's resolution.
      *
      * It is not set when even the first target is unknown.
      */
@@ -37,54 +42,36 @@ export interface IPropResolution {
  * @param props
  */
 export function resolveProp(this: any, obj: any, props: string): IPropResolution {
-    if (props.indexOf(`.`) === -1) {
-        if (props === 'this') {
-            return {
-                chain: [props],
-                idx: 0,
-                value: this
-            };
-        }
-        if (obj === undefined) {
-            return {
-                chain: [props],
-                idx: -1
-            };
-        }
-        const v = obj[props];
-        const value = typeof v === 'function' ? v.call(obj) : v;
-        return {
-            chain: [props],
-            idx: 0,
-            target: obj,
-            value
-        };
-    }
     const chain = props.split(`.`);
-    let value = obj, target, i;
-    for (i = 0; i < chain.length; i++) {
+    const len = chain.length;
+    let target = obj, value, i;
+    for (i = 0; i < len; i++) {
         const name = chain[i];
-        if (!name) {
-            return {
-                chain,
-                idx: i
-            };
-        }
-        if (!i && name === 'this') {
-            target = this;
-            value = this;
-            continue;
-        }
-        if (i < chain.length - 1) {
-            target = value;
+        switch (name) {
+            case '':
+                // name cannot be empty:
+                return {chain, idx: i - 1, errMsg: `empty name not allowed`};
+            case 'this':
+                if (i) {
+                    // 'this' can only be in the beginning:
+                    return {chain, idx: i - 1, errMsg: `'this' can only be at start`};
+                }
+                target = this;
+                value = this;
+                continue;
+            default:
+                break;
         }
         const v = target[name];
         value = typeof v === 'function' ? v.call(obj) : v;
         if (value === undefined) {
-            break;
+            continue;
+        }
+        if (i < len - 1) {
+            target = value;
         }
     }
-    if (i === chain.length) {
+    if (i === len) {
         return {
             chain,
             idx: i - 1,
@@ -94,6 +81,10 @@ export function resolveProp(this: any, obj: any, props: string): IPropResolution
     }
     return {
         chain,
-        idx: i
+        idx: i - 1
     };
+}
+
+export function verboseParse() {
+    // to parse against regex, with support for '?', '[]' and 'this', 'function(values)'
 }
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -3,16 +3,21 @@ import {resolveProp as resolve} from '../src';
 
 describe('for empty string', () => {
     it('must fail correctly', () => {
-        // expect(resolve({}, '')).to.eql({chain: [''], idx: -1});
+        expect(resolve({}, '')).to.eql({chain: [''], idx: -1});
     });
 });
 
 describe('one property', () => {
     describe('this', () => {
         it('must resolve any value', () => {
-            expect(resolve.call(undefined, null, 'this')).to.eql({chain: ['this'], idx: 0, value: undefined});
-            expect(resolve.call(null, null, 'this')).to.eql({chain: ['this'], idx: 0, value: null});
-            expect(resolve.call(123, null, 'this')).to.eql({chain: ['this'], idx: 0, value: 123});
+            expect(resolve.call(undefined, null, 'this')).to.eql({
+                chain: ['this'],
+                idx: 0,
+                target: undefined,
+                value: undefined
+            });
+            expect(resolve.call(null, null, 'this')).to.eql({chain: ['this'], idx: 0, target: null, value: null});
+            expect(resolve.call(123, null, 'this')).to.eql({chain: ['this'], idx: 0, target: 123, value: 123});
         });
     });
     describe('for primitive types', () => {
@@ -31,4 +36,48 @@ describe('one property', () => {
             expect(resolve({}, 'one')).to.eql({chain: ['one'], idx: 0, target: {}, value: undefined});
         });
     });
+});
+
+describe('multiple properties', () => {
+    describe('for simple names', () => {
+        it('must resolve', () => {
+            const obj1 = {one: {two: 12}};
+            expect(resolve(obj1, 'one.two')).to.eql({chain: ['one', 'two'], idx: 1, target: obj1.one, value: 12});
+            const obj2 = {one: {two: {three: 123}}};
+            expect(resolve(obj2, 'one.two.three')).to.eql({
+                chain: ['one', 'two', 'three'],
+                idx: 2,
+                target: obj2.one.two,
+                value: 123
+            });
+        });
+    });
+    describe('for context names', () => {
+        it('must resolve one property', () => {
+            const obj1 = {one: 1};
+            expect(resolve.call(obj1, obj1, 'this.one')).to.eql({
+                chain: ['this', 'one'],
+                idx: 1,
+                target: obj1,
+                value: 1
+            });
+            expect(resolve.call(obj1, null, 'this.one')).to.eql({
+                chain: ['this', 'one'],
+                idx: 1,
+                target: obj1,
+                value: 1
+            });
+        });
+        it('must resolve any chain', () => {
+            const obj1 = {one: {two: 12}};
+            expect(resolve.call(obj1, obj1, 'this.one.two')).to.eql({
+                chain: ['this', 'one', 'two'],
+                idx: 2,
+                target: obj1.one,
+                value: 12
+            });
+        });
+
+    });
+
 });
