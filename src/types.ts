1e5d13eae7b3e05de9a4eb6de3a9abc36bab1b4f
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2020-12-06 07:24:47 +0000

    removing exists

--- src/types.ts
+++ src/types.ts
@@ -51,9 +51,4 @@ export interface IParseResult {
      * Final resolved value, if successful, or else the property is not set.
      */
     value?: any;
-
-    /**
-     * Indicates existence of property for the last resulting value.
-     */
-    exists?: boolean;
 }
