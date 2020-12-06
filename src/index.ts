01881440677f13a7d7b3d50367a32a65fbecc962
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2020-12-06 09:04:51 +0000

    add example

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export {IParseResult, ParseErrorCode} from './types';
 export {ParsePropError} from './error';
 export {resolvePath} from './resolve-path';
-export {resolveValue} from './resolve-value';
+export {resolveValue, validateResult} from './resolve-value';
--- src/resolve-value.ts
+++ src/resolve-value.ts
@@ -1,6 +1,6 @@
 import {resolvePath} from './resolve-path';
 import {ParsePropError} from './error';
-import {ParseErrorCode} from './types';
+import {IParseResult, ParseErrorCode} from './types';
 
 /**
  * High-level path-to-value resolution function, to either return the resolved value or throw a detailed error.
@@ -12,7 +12,16 @@ import {ParseErrorCode} from './types';
  * Resolution path. If the path starts with `this`, resolution is against the call context.
  */
 export function resolveValue(this: any, target: any, path: string): any {
-    const res = resolvePath.call(this, target, path);
+    return validateResult(resolvePath.call(this, target, path));
+}
+
+/**
+ * Validates IParseResult to either return the value or throw an error.
+ *
+ * @param res
+ * Result to validate.
+ */
+export function validateResult(res: IParseResult): any {
     switch (res.errorCode) {
         case ParseErrorCode.emptyName:
             throw new ParsePropError('Empty names are not allowed.', res);
@@ -28,3 +37,15 @@ export function resolveValue(this: any, target: any, path: string): any {
             return res.value;
     }
 }
+
+/*
+TODO: consider turning IParseResult into a class? To be able to call res.validate()
+  and res.getValue()?
+export function getNotMissing(this: any, target: any, path: string): any {
+    const res = resolvePath.call(this, target, path);
+    if (res.missing) {
+        throw new Error(`Property '${res.chain[res.chain.length - 1]}' doesn't exist!`);
+    }
+    return validateResult(res);
+}
+*/
